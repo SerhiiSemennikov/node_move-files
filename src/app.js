444e07e207a29155a398715beacd2d65373e6631
SerhiiSemennikov <semennik@hotmail.com>
2025-02-15 14:24:41 +0100

    copy1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
-// write code here
+/* eslint-disable no-console */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const moveFiles = async () => {
+  const args = process.argv.slice(2);
+
+  console.log(process.argv);
+
+  const [from, to] = args;
+
+  if (args.length < 2) {
+    console.error('You need fileName and destination');
+
+    return;
+  }
+
+  try {
+    const isDirectory = fs.existsSync(to) && fs.lstatSync(to).isDirectory();
+
+    const updatedPath = isDirectory ? path.join(to, path.basename(from)) : to;
+
+    fs.renameSync(from, updatedPath);
+  } catch (error) {
+    console.error('Rename error!', error);
+  }
+};
+
+moveFiles();
+
+module.exports = {
+  moveFiles,
+};
